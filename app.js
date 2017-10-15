4c2f6f21b29813a6a1625e51b5d17609899dd665
bennutech <bennutechco@gmail.com>
2017-10-15 04:29:08 -0700

    Items entered into the input field come up on the screen with the correct look, next push will add the functionality to change complete to delete onClick

--- app.js
+++ app.js
@@ -10,17 +10,40 @@ var dayName=["","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sat
 var month=["","January","February","March","April","May","June","July","August","September","October","November","December"];
 
 today = month[mm] + " " + dd + ', ' + yyyy;
-$("#dayDisplay").append(dayName[day]); 
-$("#dateDisplay").append(today);
-
+document.getElementById("dayDisplay").innerHTML= dayName[day];
+document.getElementById("dateDisplay").innerHTML= today;
 //-----------------------------------------------------------------------------
-var task=$("#task").val();
-$("input").keypress(function(e) {    //Listening for the ENTER key on the input field
-    if(e.which == 13) { 
-    console.log(task); 
-    return task;
-    }
+var i=1;
+function saveTask(e){
+    var task=document.getElementById("task").value; //get the value stored in the input 
+    var container=document.createElement("div"); //create the list element 
+    var listItem=document.createElement("div");
+    var status=document.createElement("a");
+
     
-   // $("<h3>"+task+"</h3>").appendTo(".list");
+    document.getElementById("list-space").appendChild(container); //atttach li element to ul element 
+    container.setAttribute("id","container-"+i);
+    container.setAttribute("class","container");
+    document.getElementById("container-"+i).appendChild(listItem);
+    listItem.className="item";
+    listItem.appendChild(document.createTextNode(task)); //attach the input to the li element 
+    document.getElementById("container-"+i).appendChild(status)
+    status.className="status";
+    status.setAttribute("id","status-"+i)
+    status.appendChild(document.createTextNode("COMPLETE"));
+    i++
+}
+document.addEventListener("click",function(){
+    if(this.className=="status"){
+    console.log(this);
+    alert("The complete has been clicked");
+    }
+    console.log(getElementById(this).value);
 });
+document.getElementById("task").addEventListener("keydown",function(e){
+        if(e.key=="Enter"){
+            e.preventDefault();
+            saveTask(e);            
+        }        
+    });
 
